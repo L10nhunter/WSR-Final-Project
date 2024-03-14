8b5861f63c3c969cb3b8103553ca0cba643ce49f
L10nhunter <yegera1@newpaltz.edu>
2024-03-14 11:58:44 -0400

    changed column size from half to 3/4, made the feed show the workouts in reverse order, because reverse chronological makes more sense.

--- Client/src/pages/FriendsActivity.vue
+++ Client/src/pages/FriendsActivity.vue
@@ -1,23 +1,22 @@
 <script setup lang="ts">
 import "@/assets/main.css";
 import {ref} from "vue";
-import {getWorkouts} from "@/model/workouts";
+import {Workouts, toReversed} from "@/model/workouts";
 import WorkoutBox from "@/components/WorkoutBox.vue";
 import AddWorkoutModal from "@/components/AddWorkoutModal.vue";
 import NotLoggedBox from "@/components/NotLoggedBox.vue";
 import LoggedInContent from "@/components/LoggedInContent.vue";
 
-const workouts = ref(getWorkouts());
 const showAddWorkoutModal = ref(false);
 </script>
 
 <template>
     <NotLoggedBox/>
     <LoggedInContent>
-        <div class="columns">
-            <div class="column is-half is-offset-one-quarter">
+        <div class="columns is-centered">
+            <div class="column is-three-fifths">
                 <button class="button is-primary is-fullwidth" @click="showAddWorkoutModal = true">Add Workout</button>
-                <div class="container" v-for="workout in workouts">
+                <div class="container" v-for="workout in toReversed(Workouts)">
                     <div class="workout-card">
                         <WorkoutBox v-bind="workout"/>
                     </div>
