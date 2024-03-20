e86df50394cf2850a8d129a403ae0858844a8d4f
L10nhunter <yegera1@newpaltz.edu>
2024-03-20 15:08:43 -0400

    moved the label outside the field, so it would properly format

--- Client/src/components/Fields/SignupTextField.vue
+++ Client/src/components/Fields/SignupTextField.vue
@@ -10,23 +10,25 @@ function toggleShowPassword(): void {showPassword.value = !showPassword.value;}
 </script>
 
 <template>
+    <label class="dcs" :for="forAndId(textField)">{{ textField.label }}</label>
     <div class="field pr-3" :class="{'has-addons': textField.label.includes('Password')}">
-        <label class="dcs" :for="forAndId(textField)">{{ textField.label }}</label>
         <div class="control is-expanded has-icons-left">
             <input class="input ics bordered" v-model="contents" :type="!showPassword ? textField.type : 'text'" :placeholder="textField.placeholder" :id="forAndId(textField)" :autocomplete="textField.autocomplete">
             <span class="icon is-small is-left">
                 <i :class="textField.icon"></i>
             </span>
         </div>
-        <a class="button ics" type="button" v-if="textField.label.includes('Password')"
+        <button class="button ics bordered" type="button" v-if="textField.label.includes('Password')"
            @click.prevent="toggleShowPassword()">
-            <i :class="showPassword ? 'fa-solid fa-eye-slash' : 'fa-solid fa-eye'"></i>
-        </a>
+            <span class="icon is-small is-right">
+                <i :class="showPassword ? 'fa-solid fa-eye-slash' : 'fa-solid fa-eye'"></i>
+            </span>
+        </button>
     </div>
 </template>
 
 <style scoped>
-a.button {
+button {
     border: none;
     background: none;
     cursor: pointer;
