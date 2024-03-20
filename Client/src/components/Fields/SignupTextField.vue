b601e8aa4b65bcb67b3770a39e7eb5dec16186bd
L10nhunter <yegera1@newpaltz.edu>
2024-03-20 14:54:51 -0400

    removed emits, they don't do anything. did some formatting.

--- Client/src/components/Fields/SignupTextField.vue
+++ Client/src/components/Fields/SignupTextField.vue
@@ -3,20 +3,14 @@ import {forAndId, type TextField} from "@/model/textField";
 import {ref} from "vue";
 
 const contents = defineModel();
-const emits = defineEmits<{
-    (event: 'change', value: unknown): void;
-}>();
 const textField = defineProps<TextField>();
 const showPassword = ref(false);
-
-function toggleShowPassword(): void {
-    showPassword.value = !showPassword.value;
-}
+function toggleShowPassword(): void {showPassword.value = !showPassword.value;}
 
 </script>
 
 <template>
-    <div class="field" :class="{'has-addons': textField.label.includes('Password')}">
+    <div class="field pr-3" :class="{'has-addons': textField.label.includes('Password')}">
         <label class="dcs" :for="forAndId(textField)">{{ textField.label }}</label>
         <div class="control is-expanded has-icons-left">
             <input class="input ics bordered" v-model="contents" :type="!showPassword ? textField.type : 'text'" :placeholder="textField.placeholder" :id="forAndId(textField)" :autocomplete="textField.autocomplete">
@@ -32,10 +26,6 @@ function toggleShowPassword(): void {
 </template>
 
 <style scoped>
-div.field {
-    padding-right: 8px;
-}
-
 a.button {
     border: none;
     background: none;
