29eaedee817f6daac5659206bb0c467b9bcf66da
L10nhunter <yegera1@newpaltz.edu>
2024-03-21 10:14:01 -0400

    added toastification to the project.

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -3,9 +3,27 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Toast from "vue-toastification";
 
 const app = createApp(App)
 
-app.use(router)
+const options = {
+    position: 'top-right',
+    timeout: 5000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: true,
+    rtl: false
+}
 
-app.mount('#app')
+app
+    .use(router)
+    .use(Toast, options)
+
+    .mount('#app')
