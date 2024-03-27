d1990bd6ca11876a950793e4e0f82edf4c2bc181
L10nhunter <yegera1@newpaltz.edu>
2024-03-27 00:16:09 -0400

    added cors to the repo, because it was complaining that it didnt want to access the server without it.

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const PORT = !isNaN(parseInt(process.env.PORT)) ? parseInt(process.env.PORT) : 3000;
-app.get('/', (req, res) => {
-    res.send('Home Page');
-});
 
-app.use('/users', require('./Controllers/users'));
-app.use('/workouts', require('./Controllers/workouts'));
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200
+}));
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+app.use('/api/v1/users', require('./Controllers/users'));
+app.use('/api/v1/workouts', require('./Controllers/workouts'));
+
