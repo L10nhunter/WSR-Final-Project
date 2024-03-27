324f4a99fb291b9ac6a27d79700b2b4471cb9dcb
L10nhunter <yegera1@newpaltz.edu>
2024-03-27 17:15:26 -0400

    more logging and clearer naming.

--- Client/src/model/rest.ts
+++ Client/src/model/rest.ts
@@ -1,6 +1,10 @@
 const API_ROOT = "http://localhost:3000/api/v1";
 
 async function rest(url: string, body?: unknown, method?: string, headers?: any) {
+    console.log("rest.ts rest url: " + url)
+    console.log("rest.ts rest body: " + JSON.stringify(body))
+    console.log("rest.ts rest method: " + method)
+    console.log("rest.ts rest headers: " + headers)
     return await fetch(url, {
         method: method ?? (body ? "POST" : "GET"),
         headers: {
@@ -16,6 +20,6 @@ async function rest(url: string, body?: unknown, method?: string, headers?: any)
 
 }
 
-export async function api(action: string, body?: unknown, method?: string, headers?: any) {
-    return await rest(`${API_ROOT}/${action}`, body, method, headers);
+export async function api(endpointURL: string, body?: unknown, method?: string, headers?: any) {
+    return await rest(`${API_ROOT}/${endpointURL}`, body, method, headers);
 }
