67404ce096fc067df3e0521abd39aaa62e01cf38
L10nhunter <yegera1@newpaltz.edu>
2024-03-27 17:25:10 -0400

    changed the get to work with query instead of params. just easier. also, where params are necessary, made it look better

--- Server/Controllers/workouts.js
+++ Server/Controllers/workouts.js
@@ -5,8 +5,8 @@ app
     .get('/', (req, res) => {
         workouts.getAll().then(workouts => res.send(workouts)).catch(err => res.status(500).send(err));
     })
-    .get('/:userid', (req, res) => {
-        workouts.getWorkoutsByUser(parseInt(req.params["userid"])).then(userWorkouts => userWorkouts.length > 0 ? res.send(userWorkouts) : res.status(404).send('User not found')).catch(err => res.status(500).send(err));
+    .get('/user',  (req, res) => {
+        workouts.getWorkoutsByUser(parseInt(req.query.uid)).then(byUser => byUser ? res.send(byUser) : res.status(404).send("User not found")).catch(err => res.status(500).send(err));
     })
     .get('/search', (req, res) => {
         workouts.search(req.query.q).then(workouts => res.send(workouts)).catch(err => res.status(500).send(err));
@@ -16,10 +16,10 @@ app
         workouts.addNewWorkout(req.body).then(workout => res.send(workout)).catch(err => res.status(500).send(err));
     })
     .patch('/:id', (req, res) => {
-        workouts.edit(parseInt(req.params["id"]), req.body).then(workout => workout ? res.send(workout) : res.status(404).send('Workout not found')).catch(err => res.status(500).send(err));
+        workouts.edit(parseInt(req.params.id), req.body).then(workout => workout ? res.send(workout) : res.status(404).send('Workout not found')).catch(err => res.status(500).send(err));
     })
     .delete('/:id', (req, res) => {
-        workouts.remove(parseInt(req.params["id"])).then(workout => workout ? res.send(workout) : res.status(404).send('Workout not found')).catch(err => res.status(500).send(err));
+        workouts.remove(parseInt(req.params.id)).then(workout => workout ? res.send(workout) : res.status(404).send('Workout not found')).catch(err => res.status(500).send(err));
     });
 
 module.exports = app;
