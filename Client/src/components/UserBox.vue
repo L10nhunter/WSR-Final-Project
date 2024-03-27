bb699fc24d4468be9c70a08d107245685191189a
L10nhunter <yegera1@newpaltz.edu>
2024-03-27 17:20:28 -0400

    create component for showing the results of searches.

---
+++ Client/src/components/UserBox.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type {User} from "@/model/users";
+
+const user = defineProps<User>()
+
+</script>
+
+<template>
+    <div class="media box">
+        <div class="media-left">
+            <figure class="image is-64x64">
+                <img :src="user.image" alt="Image">
+            </figure>
+        </div>
+        <div class="media-content">
+            <p class="title is-4">{{user.username}}</p>
+            <p class="subtitle is-6">{{user.email}}</p>
+
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
