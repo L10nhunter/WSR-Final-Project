fe38967d13c55397bd6b99f41291d434f704b950
L10nhunter <yegera1@newpaltz.edu>
2024-04-03 22:16:35 -0400

    add all missing crud methods. added js docs to specify typing in data.items.

--- Server/models/users.js
+++ Server/models/users.js
@@ -1,7 +1,7 @@
-/**
- * @typedef {import('../../Client/src/model/users').User} User
- * @typedef {import('../../Client/src/model/users').newUser} newUser
- * @typedef {Object} data
+/**@typedef {import('../../Client/src/model/users').User} User*/
+/**@typedef {import('../../Client/src/model/users').newUser} newUser*/
+ /**
+ * @typedef {Object}
  * @property {User[]} items
  * @property {number} total
  * @property {number} skip
@@ -13,53 +13,28 @@ const data = require('../data/users.json');
  * @returns {Promise<User[]>}
  */
 async function getAll() {
-    return await data.items.map(item => ({
+    /**@type {User[]}*/
+    const users = await data.items;
+    return users.map(item => ({
         id: item.id,
         firstName: item.firstName,
         lastName: item.lastName,
         email: item.email,
+        phone: item.phone,
         username: item.username,
         admin: item.admin,
         friends: item.friends
     }));
 }
-
-/**
- * @param {number} id
- * @returns Promise<User>
- */
-async function get(id) {
-    return await data.items.find(item => item.id === id);
-}
-
-/**
- * @param {string} q
- * @returns {Promise<User[]>}
- */
-async function search(q) {
-    return await data.items.filter(item =>
-        new RegExp(q, 'i').test(item.firstName) ||
-        new RegExp(q, 'i').test(item.lastName) ||
-        new RegExp(q, 'i').test(item.email));
-}
-
-/**
- * @param {number} id
- * @returns Promise<User>
- */
-async function remove(id) {
-    /** @type {User} */
-    const user = await get(id);
-    data.items.splice(data.items.findIndex(item => item.id === id), 1);
-    return user;
-}
-
 /**
  * @param {newUser} inputInfo
  * @returns {Promise<User>}
  */
-async function addNewUser(inputInfo) {
+async function create(inputInfo) {
+    /**@type {User[]}*/
     const users = await data.items
+    if (users.some(user => user.email === inputInfo.email)) throw new Error('Email already exists');
+    if (users.some(user => user.username === inputInfo.username)) throw new Error('Username already exists');
     /** @type {User} */
     const newUser = {
         id: users[users.length - 1].id + 1,
@@ -67,10 +42,62 @@ async function addNewUser(inputInfo) {
         ...inputInfo,
         friends: []
     };
-
-    data.push(newUser);
+    data.items.push(newUser);
     return newUser;
 }
+
+/**
+ * @param {number} id
+ * @returns Promise<User>
+ */
+async function get(id) {
+    /**@type {User[]}*/
+    const users = await data.items
+    const user = users.find(item => item.id === id);
+    if (!user) throw new Error('User not found');
+    return users.find(item => item.id === id);
+}
+
+/**
+ * @param {string} q
+ * @returns {Promise<User[]>}
+ */
+async function search(q) {
+    /**@type {User[]}*/
+    const users = await data.items
+    return users.filter(item =>
+        new RegExp(q, 'i').test(item.firstName) ||
+        new RegExp(q, 'i').test(item.lastName) ||
+        new RegExp(q, 'i').test(item.email));
+}
+
+/**
+ * @param {number} id
+ * @param {newUser} inputInfo
+ * @returns {Promise<User>}
+ */
+async function update(id, inputInfo) {
+    /**@type {User[]}*/
+    const users = await data.items;
+    const user = users.find(item => item.id === id);
+    if (!user) throw new Error('User not found');
+    for (let key in inputInfo) user[key] = inputInfo[key];
+    return user;
+
+}
+
+/**
+ * @param {number} id
+ * @returns Promise<User>
+ */
+async function destroy(id) {
+    /** @type {User} */
+    const user = await get(id);
+    if(!user) throw new Error('User not found');
+    data.items.splice(data.items.findIndex(item => item.id === id), 1);
+    return user;
+}
+
 
 /**
  * @param {string} emailOrUsername
@@ -78,10 +105,11 @@ async function addNewUser(inputInfo) {
  * @returns {Promise<User>}
  */
 async function login(emailOrUsername, password) {
+    /**@type {User[]}*/
     const users = await data.items;
     const user = users.find(user => (user.email === emailOrUsername || user.username === emailOrUsername));
-    if (!user || user.password !== password) return users.find(user => (user.id === -1));
+    if (!user || user.password !== password) throw new Error('Invalid login credentials. Please try again.');
     return user;
 }
 
-module.exports = {getAll, get, remove, search, addNewUser, login}
+module.exports = {create, getAll, get, search, update, destroy, login}
