a749e006666e7fa03c66542c43c816a0b98dfbce
L10nhunter <yegera1@newpaltz.edu>
2024-04-04 22:58:13 -0400

    changed all the paths to use the pattern i made up in the user side.

--- Server/Controllers/workouts.js
+++ Server/Controllers/workouts.js
@@ -5,25 +5,32 @@ app
     .get('/', (req, res) => {
         workouts.getAll()
             .then(workouts => res.send(workouts))
-            .catch(err => res.status(500).send(err));
+            .catch(err => res.status(err.cause.status).send(err.message));
     })
     .get('/user',  (req, res) => {
         workouts.getWorkoutsByUser(parseInt(req.query.uid))
-            .then(byUser => byUser ? res.send(byUser) : res.status(404).send("User not found"))
-            .catch(err => res.status(500).send(err));
+            .then(byUser => res.send(byUser))
+            .catch(err => res.status(err.cause.status).send(err.message));
     })
     .get('/search', (req, res) => {
-        workouts.search(req.query.q).then(workouts => res.send(workouts)).catch(err => res.status(500).send(err));
+        workouts.search(req.query.q)
+            .then(workouts => res.send(workouts))
+            .catch(err => res.status(err.cause.status).send(err.message));
     })
-
     .post('/', (req, res) => {
-        workouts.create(req.body).then(workout => res.send(workout)).catch(err => res.status(500).send(err));
+        workouts.create(req.body)
+            .then(workout => res.send(workout))
+            .catch(err => res.status(err.cause.status).send(err.message));
     })
     .patch('/:id', (req, res) => {
-        workouts.update(parseInt(req.params.id), req.body).then(workout => workout ? res.send(workout) : res.status(404).send('Workout not found')).catch(err => res.status(500).send(err));
+        workouts.update(parseInt(req.params.id), req.body)
+            .then(workout => res.send(workout))
+            .catch(err => res.status(err.cause.status).send(err.message));
     })
     .delete('/:id', (req, res) => {
-        workouts.destroy(parseInt(req.params.id)).then(workout => workout ? res.send(workout) : res.status(404).send('Workout not found')).catch(err => res.status(500).send(err));
+        workouts.destroy(parseInt(req.params.id))
+            .then(workout => res.send(workout))
+            .catch(err => res.status(err.cause.status).send(err.message));
     });
 
 module.exports = app;
