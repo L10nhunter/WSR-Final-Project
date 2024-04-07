30bb2d390f65f36465cdc1b3f0d5f96593592e4a
L10nhunter <yegera1@newpaltz.edu>
2024-04-07 19:33:16 -0400

    removed extra brackets that weren't doing anything

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -8,7 +8,7 @@ import VueRouter from 'unplugin-vue-router/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        VueRouter({}),
+        VueRouter(),
         vue(),
         vueJsx(),
     ],
