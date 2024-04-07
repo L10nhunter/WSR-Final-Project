8b31e41e0ba57e6fd8887bfcc769a0ce152fb221
L10nhunter <yegera1@newpaltz.edu>
2024-04-07 19:42:54 -0400

    added a really bad loading bar. gotta get that fixed

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -2,19 +2,25 @@
 import "bulma/css/bulma.css";
 import NavBar from "@/components/NavBar.vue";
 import {RouterView} from 'vue-router';
+import {getSession} from "@/model/session";
 </script>
 
 <template>
-    <Suspense>
-        <body class="has-navbar-fixed-top">
-        <div id="app">
-            <NavBar/>
+
+    <body class="has-navbar-fixed-top">
+    <div id="app">
+        <NavBar/>
+        <div class="">
+            <progress class="progress is-primary is-small" max="100" v-if="getSession().loading">15%</progress>
+        </div>
+        <Suspense>
             <div class="container" id="site-container">
                 <RouterView/>
             </div>
-        </div>
-        </body>
-    </Suspense>
+        </Suspense>
+    </div>
+    </body>
+
 </template>
 
 <style scoped>
@@ -29,6 +35,11 @@ import {RouterView} from 'vue-router';
     padding-top: 1.25rem;
     display: flex;
     flex-direction: column;
+}
+
+.progress.is-primary:indeterminate {
+    background-color: var(--color-background-mute);
+    background-image: linear-gradient(to right, var(--color-background-soft) 0% 10%, var(--color-primary) 15%, var(--color-background-soft) 20% 30%, var(--color-background-mute));
 }
 </style>
 ```
