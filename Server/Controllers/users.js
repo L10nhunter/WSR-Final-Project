112900ec5a25122dae6a8c7cc5a76cf36cba8153
L10nhunter <yegera1@newpaltz.edu>
2024-04-07 20:40:04 -0400

    finally and next blocks put in.

--- Server/Controllers/users.js
+++ Server/Controllers/users.js
@@ -4,41 +4,48 @@ const {ObjectId} = require('../models/mongo');
 const app = express.Router();
 
 app
-    .get('/', (req, res) => {
+    .get('/', (req, res, next) => {
         users.getAll()
             .then(users => res.send(users))
-            .catch(err => res.status(err.cause.status).send({message: err.message}));
+            .catch(err => res.status(err.cause.status).send({message: err.message}))
+            .finally(next);
     })
-    .get('/search', (req, res) => {
+    .get('/search', (req, res, next) => {
         users.search(req.query.q)
             .then(users => res.send(users))
-            .catch(err => res.status(err.cause.status).send({message: err.message}));
+            .catch(err => res.status(err.cause.status).send({message: err.message}))
+            .finally(next);
     })
-    .post('/', (req, res) => {
+    .post('/', (req, res, next) => {
         users.create(req.body)
             .then(user => res.send(user))
-            .catch(err => res.status(err.cause.status).send({message: err.message}));
+            .catch(err => res.status(err.cause.status).send({message: err.message}))
+            .finally(next);
     })
-    .post('/login', (req, res) => {
+    .post('/login', (req, res, next) => {
         users.login(req.body.emailOrUsername, req.body.password)
             .then(user => res.send(user))
-            .catch(err => res.status(err.cause.status).send({message: err.message}));
+            .catch(err => res.status(err.cause.status).send({message: err.message}))
+            .finally(next);
     });
 
 app
-    .get('/:id', (req, res) => {
+    .get('/:id', (req, res, next) => {
         users.get(new ObjectId(req.params["id"]))
             .then(user => res.send(user))
-            .catch(err => res.status(err.cause.status).send({message: err.message}));
+            .catch(err => res.status(err.cause.status).send({message: err.message}))
+            .finally(next);
     })
-    .patch('/:id', (req, res) => {
+    .patch('/:id', (req, res, next) => {
         users.update(new ObjectId(req.params["id"]), req.body)
             .then(user => res.send(user))
-            .catch(err => res.status(err.cause.status).send({message: err.message}));
+            .catch(err => res.status(err.cause.status).send({message: err.message}))
+            .finally(next);
     })
-    .delete('/:id', (req, res) => {
+    .delete('/:id', (req, res, next) => {
         users.destroy(new ObjectId(req.params["id"]))
             .then(user => res.send(user))
-            .catch(err => res.status(err.cause.status).send({message: err.message}));
+            .catch(err => res.status(err.cause.status).send({message: err.message}))
+            .finally(next);
     });
 module.exports = app;
--- Server/Controllers/workouts.js
+++ Server/Controllers/workouts.js
@@ -1,37 +1,50 @@
 const express = require('express');
-const app = express.Router();
 const workouts = require('../models/workouts');
-const {ObjectId} = require("mongodb");
+const {ObjectId} = require('mongodb');
+const app = express.Router();
 app
-    .get('/', (req, res) => {
+    .get('/', (req, res, next) => {
         workouts.getAll()
             .then(workouts => res.send(workouts))
-            .catch(err => res.status(err.cause.status).send(err.message));
+            .catch(err => res.status(err.cause.status).send(err.message))
+            .finally(next);
     })
-    .get('/user',  (req, res) => {
-        workouts.getWorkoutsByUser(new ObjectId(req.query.uid))
-            .then(byUser => res.send(byUser))
-            .catch(err => res.status(err.cause.status).send(err.message));
-    })
-    .get('/search', (req, res) => {
+    .get('/search', (req, res, next) => {
         workouts.search(req.query.q)
             .then(workouts => res.send(workouts))
-            .catch(err => res.status(err.cause.status).send(err.message));
+            .catch(err => res.status(err.cause.status).send(err.message))
+            .finally(next);
     })
-    .post('/', (req, res) => {
+    .post('/', (req, res, next) => {
         workouts.create(req.body)
             .then(workout => res.send(workout))
-            .catch(err => res.status(err.cause.status).send(err.message));
+            .catch(err => res.status(err.cause.status).send(err.message))
+            .finally(next);
     })
-    .patch('/:id', (req, res) => {
+    .get('/user/:uid',  (req, res, next) => {
+        workouts.getWorkoutsByUser(new ObjectId(req.params.uid))
+            .then(byUser => res.send(byUser))
+            .catch(err => res.status(err.cause.status).send(err.message))
+            .finally(next);
+    })
+    .get('/:id', (req, res, next) => {
+        workouts.get(new ObjectId(req.params.id))
+            .then(workout => res.send(workout))
+            .catch(err => res.status(err.cause.status).send(err.message))
+            .finally(next);
+    })
+    .patch('/:id', (req, res, next) => {
         workouts.update(new ObjectId(req.params.id), req.body)
             .then(workout => res.send(workout))
-            .catch(err => res.status(err.cause.status).send(err.message));
+            .catch(err => res.status(err.cause.status).send(err.message))
+            .finally(next);
     })
-    .delete('/:id', (req, res) => {
+    .delete('/:id', (req, res, next) => {
         workouts.destroy(new ObjectId(req.params.id))
             .then(workout => res.send(workout))
-            .catch(err => res.status(err.cause.status).send(err.message));
-    });
+            .catch(err => res.status(err.cause.status).send(err.message))
+            .finally(next);
+    })
+
 
 module.exports = app;
