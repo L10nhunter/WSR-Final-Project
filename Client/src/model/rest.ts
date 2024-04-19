58bd2cf3fb4b7eedac9fd57fe77552ab53760c63
L10nhunter <yegera1@newpaltz.edu>
2024-04-19 17:32:02 -0400

    lots of refactoring.

--- Client/src/model/rest.ts
+++ Client/src/model/rest.ts
@@ -1,28 +1,45 @@
-import {showError} from "@/model/session";
+import {useToast} from "vue-toastification";
+import {getSession} from "@/model/session";
+import {MyError} from "@/model/MyError";
+import { StatusCodes } from "http-status-codes";
+import type {DynamicDataEnvelope} from "@/model/TransferTypes";
 
 const API_ROOT: string = "http://localhost:3000/api/v1";
 
 async function rest(url: string, body?: unknown, method?: string, headers?: any) {
-    console.log("rest.ts rest url: " + url)
-    console.log("rest.ts rest body: " + JSON.stringify(body))
-    console.log("rest.ts rest method: " + method)
-    console.log("rest.ts rest headers: " + headers)
+    const session = getSession();
+    session.loading++;
+    console.debug("rest.ts rest url: " + url)
+    console.debug("rest.ts rest body: " + JSON.stringify(body))
+    console.debug("rest.ts rest method: " + method)
+    console.debug("rest.ts rest headers: " + headers)
     return await fetch(url, {
         method: method ?? (body ? "POST" : "GET"),
         headers: {
             'Content-Type': 'application/json',
             ...headers
         },
-        body: body ? JSON.stringify(body) : undefined
+        body: JSON.stringify(body)
     })
         .then(response => response.ok
             ? response.json()
             : response.json().then(err => {
-                    showError(err, true,{code: response.status, message: response.statusText})
-                    throw err;
-            }));
+                showError(new MyError(response.status, err.message))
+            }))
+        .catch(() => {})
+        .finally(() => session.loading--);
 }
 
-export async function api(endpointURL: string, body?: unknown, method?: string, headers?: any) {
-    return await rest(`${API_ROOT}/${endpointURL}`, body, method, headers).catch(() => {}); // return empty object on error
+export async function api<T>(endpointURL: string, body?: unknown, method?: string, headers?: any) : Promise<DynamicDataEnvelope<T>> {
+    return await rest(`${API_ROOT}/${endpointURL}`, body, method, headers)
+        .catch(() => {});
+}
+
+function showError(error: MyError): void {
+    const toast = useToast();
+    error.locationData
+        ? console.error("Error " + error.code + " (" + StatusCodes[error.code] + "):\nError Message: " + error.message + "\nthrown from " + error.locationData.fileName + " at line " + error.locationData.lineNum)
+        : console.error("Error " + error.code + " (" + StatusCodes[error.code] + "):\nError Message: " + error.message + "\nthrown from rest.ts");
+    getSession().messages.push({type: "error", message: error.message});
+    toast.error(error.message);
 }
--- Client/src/model/session.ts
+++ Client/src/model/session.ts
@@ -2,7 +2,7 @@ import {reactive} from "vue";
 import {useRouter} from "vue-router";
 import {useToast} from "vue-toastification";
 import {showLoginModal, type User} from "@/model/users";
-import * as rest from "./rest";
+import {api} from "@/model/rest";
 import type {ObjectId} from "mongodb";
 
 const session = reactive({
@@ -13,19 +13,8 @@ const session = reactive({
         type: string,
         message: string
     }[],
-    loading: false
+    loading: 0
 });
-
-export async function api(action: string, body?: unknown, method?: string, headers?: any) {
-    console.log("session.ts api action: " + action);
-    console.log("session.ts api body: " + JSON.stringify(body));
-    console.log("session.ts api method: " + method);
-    console.log("session.ts api headers: " + headers);
-    session.loading = true;
-    return await rest.api(`${action}`, body, method, headers)
-        .catch(() => {})
-        .finally(() => session.loading = false);
-}
 
 export function getSession() {
     return session;
@@ -49,15 +38,15 @@ export function useLogin() {
     const router = useRouter();
     return {
         async login(emailOrUsername: string, password: string): Promise<User> {
-            return await api("users/login", {emailOrUsername, password}, "POST")
-                .then((user: User) => {
+            return await api<User>("users/login", {emailOrUsername, password}, "POST")
+                .then((response ) => {
+                    const user : User = response.data;
                     if (!user) throw new Error("Invalid login credentials. Please try again.");
                     session.user = user;
                     showLoginModal.value = false;
                     router.push(session.redirectURL ?? "/").then((r) => r);
-                    const toast= useToast();
-                    toast.success("Welcome " + user.firstName + " " + user.lastName + "!\nYou are now logged in.");
-                    console.log("User " + user.firstName + " " + user.lastName + " logged in.");
+                    useToast().success("Welcome " + user.firstName + " " + user.lastName + "!\nYou are now logged in.");
+                    console.debug("User " + user.firstName + " " + user.lastName + " logged in.");
                     return session.user;
                 })
                 .catch((err)=>{throw err}) as User;
@@ -70,16 +59,4 @@ export function useLogin() {
             router.push('/').then((r) => r);
         }
     }
-}
-
-interface Status {
-    code: number;
-    message: string;
-}
-
-export function showError(error: any, consoleOnly?: boolean, status?: Status):void {
-    const toast= useToast();
-    status ? console.error("Error " + status.code + " (" + status.message + ") " + error.message) : console.error(error);
-    session.messages.push({type: "error", message: error.message});
-    if(!consoleOnly) toast.error(error.message);
 }
