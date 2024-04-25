59dbfc30f1dc3f3fb7cbe430b70a6d69c712271b
L10nhunter <yegera1@newpaltz.edu>
2024-04-25 19:08:10 -0400

    Temp profile page set up. still needs lots and lots of work.

--- Client/src/pages/profile.vue
+++ Client/src/pages/profile.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
+import {updateUser, type User} from "@/model/users";
+import {ref} from "vue";
+import {getUser} from "@/model/session";
+import {definePage} from "vue-router/auto";
+import EditUserTextField from "@/components/Fields/EditUserTextField.vue";
+import {getTextField} from "@/model/textField";
 
+definePage({
+    meta: {
+        requiresAuth: true,
+    }
+});
+const user = ref<User>(getUser()!);
+console.log(user);
+
+const textFields = [
+    {field: getTextField("First Name"), model: user.value.firstName},
+    {field: getTextField("Last Name"), model: user.value.lastName},
+    {field: getTextField("EmailReg"), model: user.value.email},
+    {field: getTextField("Phone"), model: user.value.phone},
+    {field: getTextField("Username"), model: user.value.username},
+    {field: getTextField("PasswordLogin"), model: user.value.password},
+];
+//TODO: fix how broken everything is.
 </script>
 
 <template>
-temp for profile
+    <div class="box dcs bordered">
+        <div class="form">
+            <EditUserTextField v-for="text in textFields" v-bind="text.field" :v-model="text.model"/>
+        </div>
+        <button class="button is-primary" @submit.prevent @click="updateUser(user)">Edit User</button>
+        <button class="button has-text-weight-bold">Cancel</button>
+    </div>
 </template>
 
 <style scoped>
